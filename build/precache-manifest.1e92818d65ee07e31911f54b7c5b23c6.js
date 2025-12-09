7b3d292f138e1d19809afc9105fdc9146e184459
st0rmbtw <>
2025-12-09 16:18:43 +0300

    Init

---
+++ build/precache-manifest.1e92818d65ee07e31911f54b7c5b23c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2824af94cca1b3809cac5c17758c26b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2d4ccf8a8956bf148e",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "9d2d4ccf8a8956bf148e",
+    "url": "/static/js/main.0402dfe2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
