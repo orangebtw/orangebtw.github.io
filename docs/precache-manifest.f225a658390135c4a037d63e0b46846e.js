0a05dab16b0034077587ddb51c255eeaf2b8caf2
st0rmbtw <>
2025-12-26 11:50:07 +0300

    Build

---
+++ docs/precache-manifest.f225a658390135c4a037d63e0b46846e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a77db7012edbc587a15ad847211e57d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2d4ccf8a8956bf148e",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "9d2d4ccf8a8956bf148e",
+    "url": "/static/js/main.0402dfe2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
